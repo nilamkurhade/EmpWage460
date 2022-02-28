dbd35fe139afe73355d4b1e9d39df6b43d982fce
nilamkurhade <nilamkurhade87@gmail.com>
2022-02-28 16:18:14 +0530

    added: checked emp is present or absent

--- empWageComp.sh
+++ empWageComp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to emp wage computation"
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
